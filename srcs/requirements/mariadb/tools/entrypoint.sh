2d9a1480c0001bf48d874d1ef49a9a6c4bbadfbb
Sacrifist13 <kev.kraft13@gmail.com>
2026-04-15 15:38:45 +0200

    Inception

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -5,9 +5,22 @@ chown -R mysql:mysql /run/mysqld
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
 
-chown -R mysql:mysql /var/lib/mysql
-mariadb-install-db --user=mysqld --basedir=/usr --datadir=/var/lib/mysql > /dev/null
+    chown -R mysql:mysql /var/lib/mysql
+    mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql > /dev/null
 
-cat << EOF > /tmp/init.sql
+    cat << EOF > /tmp/init.sql
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES;
 EOF
+
+    mariadbd --user=mysql --bootstrap < /tmp/init.sql
+    rm -f /tmp/init.sql
+
 fi
+
+exec mariadbd --user=mysql
+
