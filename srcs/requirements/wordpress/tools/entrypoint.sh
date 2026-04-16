2e45408e04018e73697896f39541acdb6c7fbc78
Sacrifist13 <kev.kraft13@gmail.com>
2026-04-16 17:59:31 +0200

    Inception

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -2,8 +2,32 @@
 
 cd /var/www/wordpress
 
-if [ ! -f wp-config.php ]; then
+if [ ! -f "wp-config.php" ]; then
 
 wp core download --allow-root
 
+wp config create \
+    --dbname="${MYSQL_DATABASE}" \
+    --dbuser="${MYSQL_USER}" \
+    --dbpass="${MYSQL_PASSWORD}" \
+    --dbhost="mariadb" \
+    --allow-root
+
+wp core install \
+    --url="${DOMAIN_NAME}" \
+    --title="${WP_TITLE}" \
+    --admin_user="${WP_ADMIN_USER}" \
+    --admin_password="${WP_ADMIN_PASSWORD}" \
+    --admin_email="${WP_ADMIN_EMAIL}" \
+    --skip-email \
+    --allow-root
+
+wp user create \
+    "${WP_USER}" \
+    "${WP_USER_EMAIL}" \
+    --role=author \
+    --user_pass="${WP_USER_PASSWORD}" \
+    --allow-root
 fi
+
+exec php-fpm8.2 -F
